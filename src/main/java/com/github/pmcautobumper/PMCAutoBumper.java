744660b096f2e052df561fb0a5f562b03961f9aa
gv1222 <gv1222MC@gmail.com>
2014-01-19 01:52:37 -0500

    Updated and hopefully fixed

--- src/main/java/com/github/pmcautobumper/PMCAutoBumper.java
+++ src/main/java/com/github/pmcautobumper/PMCAutoBumper.java
@@ -198,7 +198,7 @@ public class PMCAutoBumper extends JavaPlugin
 	private void enableWebClient()
 	{
 		//Arbitrary choice of browser
-		webClient = new WebClient(BrowserVersion.FIREFOX_17);
+        webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER_10);
 		//This gives time for the javascript to load. If we don't allow it to load, clicking the bump button fails
 		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
 		//Since we're giving time for javascript to load, we obviously want javascript enabled as well
